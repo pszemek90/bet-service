fe2603a5e9d5efd34eb3d118a3c7504af3302849
pszemek <przemyslaw.miekos@gmail.com>
2025-03-12 06:25:58 +0100

    dummy endpoints for performance testing added

--- src/main/java/com/prezcode/betservice/controller/BetController.java
+++ src/main/java/com/prezcode/betservice/controller/BetController.java
@@ -8,13 +8,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 @RestController
 @RequestMapping("/bets")
 public class BetController {
+
+    private List<byte[]> memoryLeakList = new ArrayList<>();
 
     @GetMapping("/health")
     public Mono<ResponseEntity<String>> healthcheck(@RequestHeader("X-correlation-id") String correlationId) {
         MDC.put("correlationId", correlationId);
         return Mono.just(ResponseEntity.ok("Health is fine"));
+    }
+
+    @GetMapping("/cpu")
+    public Mono<Void> cpuIntensiveTask() {
+        IntStream.range(0, 10_000).parallel().forEach(i -> {
+            double result = Math.tan(Math.random());
+        });
+        return Mono.empty();
+    }
+
+    @GetMapping("/memory")
+    public Mono<Void> memoryLeak() {
+        memoryLeakList.add(new byte[128 * 1024]);
+        return Mono.empty();
     }
 }
